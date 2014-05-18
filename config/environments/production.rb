eb5a44b05b117af9057be878fe7ebcf5fc564f4d
John Baro <johnbaro@gmail.com>
2014-05-18 18:29:33 +1000

    Comment out "dump_schema_after_migrate"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,6 @@ DemoApp::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # - Fails on Heroku 
+  #config.active_record.dump_schema_after_migration = false
 end
